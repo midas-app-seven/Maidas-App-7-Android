e1f1edd87f902c87d2df510f215ba0eb7d5324e3
공도윤 <ddddy0114@gmail.com>
2022-11-03 20:46:42 +0900

    Auto stash before checking out "origin/main"

---
+++ MaidasApp7/app/src/main/java/com/junsu/maidasapp7/admin/AdminMainActivity.kt
@@ -0,0 +1,12 @@
+package com.junsu.maidasapp7.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junsu.maidasapp7.R
+
+class AdminMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_main)
+    }
+}
