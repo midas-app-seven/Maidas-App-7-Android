4f0b7424cf7dc4a9ab40a6fa1f60638c664ffa87
Junsu <krpjs0508@gmail.com>
2022-11-04 02:11:22 +0900

    .

--- MaidasApp7/app/src/main/java/com/junsu/maidasapp7/user/fragment/calendar/UserCalendarFragment.kt
+++ MaidasApp7/app/src/main/java/com/junsu/maidasapp7/user/fragment/calendar/UserCalendarFragment.kt
@@ -1,12 +1,26 @@
 package com.junsu.maidasapp7.user.fragment.calendar
 
+import android.os.Bundle
+import android.view.View
+import com.junsu.maidasapp7.AllDecorator
 import com.junsu.maidasapp7.R
+import com.junsu.maidasapp7.TodayDecorator
 import com.junsu.maidasapp7.base.BaseFragment
 import com.junsu.maidasapp7.databinding.FragmentUserCalenderBinding
 
 class UserCalendarFragment : BaseFragment<FragmentUserCalenderBinding>(
     R.layout.fragment_user_calender
 ) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.calendarUserCalendar.addDecorators(
+            AllDecorator(requireActivity()),
+            TodayDecorator(requireActivity())
+        )
+    }
+
     override fun observeEvent() {
     }
 }
